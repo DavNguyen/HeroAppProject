96b7107ad04f47e516249ebdc58f6bade492a1b5
nicknguyenl <nicholasnguyen66@gmail.com>
2021-11-17 20:21:05 -0600

    Create Hero Draft

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,70 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
+import TextField from "@material-ui/core/TextField";
+import { Button } from "@material-ui/core";
 
 function App() {
+  const [value, setValue] = useState("");
+  const handleChange = (e) => {
+    console.log(`Typed => ${e.target.value}`);
+    setValue(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <h1> Custom Hero Database Builder</h1>
+        <TextField
+          value={value}
+          onChange={handleChange}
+          label="Enter Your Hero's Name"
+          variant="outlined"
+          color="primary"
+        />
+        <TextField
+          label="Enter Your Hero's Age"
+          variant="outlined"
+          color="secondary"
+          type="email"
+          required
+        />
+        <TextField
+          label="Enter Your Hero's Gender "
+          variant="outlined"
+          color="secondary"
+          type="email"
+          required
+        />
+        <TextField
+          label="Enter Your Hero's Description"
+          variant="outlined"
+          color="secondary"
+          type="email"
+          required
+        />
+        <TextField
+          label="Enter Your Hero's Ability"
+          variant="outlined"
+          color="secondary"
+          type="email"
+          required
+        />
+        <TextField
+          label="Enter Your Hero's Health"
+          variant="outlined"
+          color="secondary"
+          type="email"
+          required
+        />
+        <TextField
+          label="Copy and Paste Your Hero's Image Link Here"
+          variant="outlined"
+          color="secondary"
+          type="email"
+          required
+        />
+        <Button variant="outlined">Create Hero</Button>
+      </div>
     </div>
   );
 }
