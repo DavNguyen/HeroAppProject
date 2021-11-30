51857bccb7ca188d702b43c425320b047d167977
nicknguyenl <nicholasnguyen66@gmail.com>
2021-11-30 17:27:23 -0600

    Draft2

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import React, { useState } from "react";
 import "./App.css";
+
+import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
-import { Button } from "@material-ui/core";
+import { Button, Grid } from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
 
 function App() {
   const [value, setValue] = useState("");
@@ -10,10 +18,21 @@ function App() {
     setValue(e.target.value);
   };
 
+  const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 140,
+    },
+  });
+
+  const classes = useStyles();
+
   return (
     <div className="App">
       <div className="App-header">
-        <h1> Custom Hero Database Builder</h1>
+        <h1> Custom Hero Database Builder </h1>
         <TextField
           value={value}
           onChange={handleChange}
@@ -63,7 +82,36 @@ function App() {
           variant="outlined"
           color="primary"
         />
-        <Button variant="outlined">Create Hero</Button>
+        <Button variant="contained" color="primary" type="submit">
+          Create Hero
+        </Button>
+
+        <Card className={classes.root}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image="/static/images/cards/contemplative-reptile.jpg"
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {value}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              Share
+            </Button>
+            <Button size="small" color="primary">
+              Learn More
+            </Button>
+          </CardActions>
+        </Card>
       </div>
     </div>
   );
