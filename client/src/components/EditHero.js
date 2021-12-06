1fec7b3327ebd1ec605d707f01d600dfb77b429b
nicknguyenl <nicholasnguyen66@gmail.com>
2021-12-05 23:12:57 -0600

    Test2

--- client/src/components/EditHero.js
+++ client/src/components/EditHero.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, Component } from "react";
 import heroCalls from "../API/heroCalls";
 import Card from "@material-ui/core/Card";
 import Typography from "@material-ui/core/Typography";
-// import Button from "@material-ui/core/Button";
+import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
 import InputLabel from "@material-ui/core/InputLabel";
