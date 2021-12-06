bd4422d91a69d74bbfd4bf75bc236480c718ede3
DavNguyen <70351432+DavNguyen@users.noreply.github.com>
2021-12-05 22:46:57 -0500

    Base FrontEnd Created

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,32 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Button from "@mui/material/Button";
+// import Button from "@mui/material/Button";
+import Homepage from "./components/Homepage";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 
 //Test Comment
 function App() {
   return (
-    <div className="App">
-      <header className="App-header-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    // <Router>
+    <div>
+      {/* <header className="App-header-header"> */}
+      {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload - Dev Branch.
-        </p>
-        <a
+        </p> */}
+      {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-        {/* <h1>Hero Project</h1> */}
-      </header>
+        </a> */}
+      {/* <h1>Hero Project</h1> */}
+      {/* </header> */}
+      <Homepage />
     </div>
+    // </Router>
   );
 }
 
